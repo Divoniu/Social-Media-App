e0ecb666c317890b13b98e2fac2560d0322a98a5
Ovidiu Nicolaescu <divoniu@yahoo.ro>
2023-03-14 16:34:08 +0200

    secured my login server

--- src/firebase.js
+++ src/firebase.js
@@ -14,19 +14,19 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA26vc7UCSgM-YIGdTwpadgLtsQkKe54l8",
+  apiKey: process.env.REACT_APP_API_KEY,
 
-  authDomain: "login-social-media-app.firebaseapp.com",
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
 
-  projectId: "login-social-media-app",
+  projectId: process.env.REACT_APP_PROJECT_ID,
 
-  storageBucket: "login-social-media-app.appspot.com",
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
 
-  messagingSenderId: "836334346160",
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 
-  appId: "1:836334346160:web:89ec84a1f414b3d36c4075",
+  appId: process.env.REACT_APP_APP_ID,
 
-  measurementId: "G-DBXVMW0XMJ",
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
